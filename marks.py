e7bf97fe0ef5621257afed60eb6a6edbb056f5ce
Michael Clermont <michael.clermont@mths.ca>
2022-06-02 18:15:03 +0000

    added marks program

---
+++ marks.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# Created by: Michael Clermont
+# Created on: June 2022
+# This program calculates the average using lists
+
+import random
+
+
+def average_of_numbers(allmarks):
+    # this function gets the average
+
+    numbers_average = 0
+    for singlemark in allmarks:
+        numbers_average = singlemark + numbers_average
+    numbers_average = numbers_average / len(allmarks)
+
+    return numbers_average
+
+
+def main():
+    # this function gets the marks
+
+    marks = []
+
+    # input
+    mark = int(input("What is your mark? (as %): "))
+    marks.append(mark)
+    while mark != -1:
+        mark = int(input("What is your mark? (as %): "))
+        marks.append(mark)
+    marks.pop()
+    print("")
+
+    average = average_of_numbers(marks)
+    print("The average is: {0}%".format(average))
+
+
+if __name__ == "__main__":
+    main()
