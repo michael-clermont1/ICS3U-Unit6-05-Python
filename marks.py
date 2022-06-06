33405f134570884f218380a79cfdae15e57d9193
Michael Clermont <michael.clermont@mths.ca>
2022-06-06 17:53:46 +0000

    added marks program:

--- marks.py
+++ marks.py
@@ -27,13 +27,17 @@ def main():
     mark = int(input("What is your mark? (as %): "))
     marks.append(mark)
     while mark != -1:
+        if mark > 100 or mark < -1:
+            marks.pop()
+            mark = int(input("That is not a valid mark, enter another mark! (as %): "))
+            marks.append(mark)
         mark = int(input("What is your mark? (as %): "))
         marks.append(mark)
     marks.pop()
     print("")
 
     average = average_of_numbers(marks)
-    print("The average is: {0}%".format(average))
+    print("The average is: {0}%.".format(average))
 
 
 if __name__ == "__main__":
